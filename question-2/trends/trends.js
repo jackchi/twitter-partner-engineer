c16b8d356e0d5cbc3743b5e1704510739d7d49ca
jackchi <fuching.chi@gmail.com>
2014-09-07 00:09:10 +0200

    Twitter Challange: Meteor now uses NPM packages

--- question-2/trends/trends.js
+++ question-2/trends/trends.js
@@ -1,15 +1,63 @@
 if (Meteor.isClient) {
-  Meteor.call("checkTwitter", function(error, results) {
-        console.log(results); //results.data should be a JSON object
+  // avail = Meteor.call("trendAvailable", function(error, results) {
+  //       console.log("ClientAvail: " + results.content); 
+  //   });
+
+  place = Meteor.call("trendPlace", 1, function(error, results) {
+        console.log("ClientPlace: " + results.content); 
     });
 
 }
 
 if (Meteor.isServer) {
-   Meteor.methods({
-        checkTwitter: function () {
-            this.unblock();
-            return Meteor.http.call("GET", "https://api.twitter.com/1.1/trends/available.json");
-        }
-    });
-}
+  // OAuth2 Tokens
+  var OAuth2 = Meteor.npmRequire('OAuth').OAuth2,
+      oauth2 = new OAuth2('oddsxvxeiaQWTj4zwhNn3JJoP', '0ffj8eeImgevD7TlAERGRzVukYo3SdA4jx0zhrqfDpgQYpzaGu', 'https://api.twitter.com/', null, 'oauth2/token', null),
+      token = "" ; 
+
+  function getToken() {
+    var token = Async.runSync(function(done) {
+        oauth2.getOAuthAccessToken('', {'grant_type': 'client_credentials'}, function (err, access_token) {
+           done(err, access_token);
+          });  
+      });
+      return token.result;
+  }
+
+  Meteor.startup(function() {
+    /* Acquire Client-Application Access Token */
+    token = getToken();
+    console.log("Token: "+token);
+  });
+
+  Meteor.methods({
+    'trendAvailable' : function() {
+      var avail = Async.runSync(function(done) {
+        Meteor.http.call(
+          "GET", 
+          "https://api.twitter.com/1.1/trends/available.json", 
+          { headers: { Authorization: 'Bearer ' + token }},
+          function (err, results) {
+            console.log("ServerAvailable: " + results.content);
+            done(err, results);
+          }
+        );
+      });  
+      return avail.result;
+    }, 
+    'trendPlace' : function(woeid) {
+      var trends = Async.runSync(function(done) {
+        Meteor.http.call(
+          "GET", 
+          "https://api.twitter.com/1.1/trends/place.json", 
+          { headers: { Authorization: 'Bearer ' + token},
+            params: { id: woeid } }, 
+          function (err, results) { 
+            console.log("ServerPlace: " + results.content);
+            done(err, results)    
+          });
+        });
+      return trends.result;
+    }
+  });
+}  
